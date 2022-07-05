ea3b5447d1599fddc5f22854c954381b62fc5730
Redevilkz <stefano.badalucco@gmail.com>
2022-07-05 20:48:00 +0200

    Create text file item component, to install tibymce.

---
+++ src/components/itemViewTypes/TextFileItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :style="`height: ${height - 14}px; width: ${itemDialog.dimension.width - 4}px; `"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+
+import MonacoEditor from "monaco-editor-vue3";
+import ItemDialog from "@/models/ItemDialog";
+import { getFileExtensionFromName } from "@/context/fileSystemController";
+
+export default defineComponent({
+  props: {
+    itemDialog: Object as PropType<ItemDialog>,
+    height: Number,
+  },
+  components: {},
+  emits: [],
+  setup(props, _) {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
