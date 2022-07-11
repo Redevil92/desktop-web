66fb64e85adf211eabba7acc489d73edd6b2f729
Redevilkz <stefano.badalucco@gmail.com>
2022-07-11 23:00:40 +0200

    Created component for displaying images. (in progress)

---
+++ src/components/itemViewTypes/ImageFileItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :style="`height: ${height - 14}px; width: ${itemDialog.dimension.width - 4}px; `">
+    <img :src="imageFile" alt="" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+
+import ItemDialog from "@/models/ItemDialog";
+import { readFile } from "@/context/fileSystemController";
+
+export default defineComponent({
+  props: {
+    itemDialog: Object as PropType<ItemDialog>,
+    height: Number,
+  },
+  components: {},
+  emits: [],
+  setup(props, _) {
+    const imageFile = ref("");
+
+    if (props.itemDialog?.name) {
+      imageFile.value = readFile(props.itemDialog?.name);
+      console.log(imageFile.value);
+    }
+
+    return { imageFile };
+  },
+});
+</script>
+
+<style scoped></style>
