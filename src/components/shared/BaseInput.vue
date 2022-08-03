cabaa001c8296649eb3fc9a45698479dbb3ecc24
Redevilkz <stefano.badalucco@gmail.com>
2022-08-03 22:53:32 +0200

    Fix on Baseinput, v-model works.

--- src/components/shared/BaseInput.vue
+++ src/components/shared/BaseInput.vue
@@ -1,18 +1,16 @@
 <template>
-  <input @input="handleInput" />
+  <input @input="changePageTitle" :value="modelValue" />
 </template>
 
 <script>
 export default {
-  prop: ["value"],
-  data() {
-    return {
-      content: this.value,
-    };
+  props: {
+    modelValue: String, // previously was `value: String`
   },
+  emits: ["update:modelValue"],
   methods: {
-    handleInput(e) {
-      this.$emit("input", this.content);
+    changePageTitle(title) {
+      this.$emit("update:modelValue", title); // previously was `this.$emit('input', title)`
     },
   },
 };
