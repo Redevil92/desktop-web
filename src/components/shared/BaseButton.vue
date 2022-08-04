50d86846722a830a55f8602949f572e32edde76a
Redevilkz <stefano.badalucco@gmail.com>
2022-08-04 17:16:04 +0200

    Added more props to base button

--- src/components/shared/BaseButton.vue
+++ src/components/shared/BaseButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="base-button" @click="clickHandler">
+  <button
+    class="base-button"
+    :class="{ 'normal-button': !small, 'small-button': small, 'neutral-color': neutralColor }"
+    @click="clickHandler"
+  >
     <slot></slot>
   </button>
 </template>
@@ -8,7 +12,10 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  props: {},
+  props: {
+    small: Boolean,
+    neutralColor: Boolean,
+  },
   components: {},
   emits: ["click"],
   setup(_, context) {
@@ -24,13 +31,29 @@ export default defineComponent({
   background-color: var(--primary-color);
   font-size: var(--medium-font-size);
   color: white;
-  padding: 7px 12px;
   outline: none;
   border: none;
+}
+
+.normal-button {
+  padding: 7px 12px;
   border-radius: 10px;
+}
+
+.small-button {
+  padding: 2px 6px;
+  border-radius: 5px;
 }
 
 .base-button:hover {
   background-color: var(--primary-color_darker);
+}
+
+.neutral-color {
+  background-color: var(--neutral-color) !important;
+}
+
+.neutral-color:hover {
+  background-color: var(--neutral-color) !important;
 }
 </style>
