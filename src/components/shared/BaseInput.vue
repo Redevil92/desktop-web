e6875d858ed6b116481999ef6544cf473b99929a
Redevilkz <stefano.badalucco@gmail.com>
2022-08-04 17:15:31 +0200

    Some fixes on base input.

--- src/components/shared/BaseInput.vue
+++ src/components/shared/BaseInput.vue
@@ -1,16 +1,18 @@
 <template>
-  <input @input="changePageTitle" :value="modelValue" />
+  <input @change="change" :type="type" :value="modelValue" />
 </template>
 
 <script>
 export default {
   props: {
     modelValue: String, // previously was `value: String`
+    type: { type: String, default: "text" }, // previously was `value: String`
   },
   emits: ["update:modelValue"],
   methods: {
-    changePageTitle(title) {
-      this.$emit("update:modelValue", title); // previously was `this.$emit('input', title)`
+    change(event) {
+      console.log(event.target.value);
+      this.$emit("update:modelValue", event.target.value); // previously was `this.$emit('input', title)`
     },
   },
 };
@@ -18,5 +20,16 @@ export default {
 
 <style lang="css" scoped>
 input {
+  border: 1px solid white;
+  background-color: var(--dialog-background-color_dark);
+  padding: 3px 8px;
+  font-size: var(--medium-font-size);
+  border-radius: 9px;
+  color: white;
+}
+
+input:focus {
+  outline: 4px solid var(--primary-color);
+  border: 1px solid var(--primary-color);
 }
 </style>
