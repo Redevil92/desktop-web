07cda7c5e08b88a77d7e336d278f9f2725f0eb5d
Redevilkz <stefano.badalucco@gmail.com>
2022-08-26 00:14:56 +0200

    Start to create component for selecting multiple element.

---
+++ src/components/shared/SelectionBoxZone.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- share state with the scoped slot -->
+    <slot :dropZoneActive="active"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    dropPath: String,
+  },
+  components: {},
+
+  setup(props, context) {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
