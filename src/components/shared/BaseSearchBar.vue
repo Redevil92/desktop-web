3258fd6956fe5da9949820070c88951c76c5959b
Redevilkz <stefano.badalucco@gmail.com>
2022-10-08 21:24:38 +0200

    Created search bar component

---
+++ src/components/shared/BaseSearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <input @change="change" :type="type" :value="modelValue" />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: String, // previously was `value: String`
+    type: { type: String, default: "text" }, // previously was `value: String`
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    change(event) {
+      this.$emit("update:modelValue", event.target.value); // previously was `this.$emit('input', title)`
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+input {
+  border: 1px solid white;
+  background-color: var(--dialog-background-color_dark);
+  padding: 3px 8px;
+  font-size: var(--medium-font-size);
+  border-radius: 9px;
+  color: white;
+}
+
+input:focus {
+  outline: 4px solid var(--primary-color);
+  border: 1px solid var(--primary-color);
+}
+</style>
