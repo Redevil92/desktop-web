7d7b0d73cb4d9ce18c0dd8917275c4a687be6a11
Redevilkz <stefano.badalucco@gmail.com>
2022-10-14 17:32:06 +0200

    Created change desktop image component

---
+++ src/components/apps/systemSettings/ChangeDesktopImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="color: white">HALLLO</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    msg: String,
+  },
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
