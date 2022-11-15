4f821c38b00b46eb6b7aebebdc1dd6dcfd561bbc
Redevilkz <stefano.badalucco@gmail.com>
2022-11-15 08:35:48 +0100

    Audio player mirrored visualizer

--- src/components/apps/audioItem/AudioPlayer.vue
+++ src/components/apps/audioItem/AudioPlayer.vue
@@ -14,6 +14,7 @@ import { readFile } from "@/context/fileSystemController";
 
 import useBase64Handler from "@/hooks/useBase64Handler";
 import { useFileSystemStore } from "@/stores/fileSystemStore";
+import { buffer } from "stream/consumers";
 
 export default defineComponent({
   props: {
@@ -29,9 +30,10 @@ export default defineComponent({
     const analyzer = ref<AnalyserNode | null>(null);
     const audioCtx = ref(new window.AudioContext());
     const audioSource = ref();
+    const x = ref(0);
 
     function animate() {
-      let x = 0;
+      x.value = 0;
       if (canvasRef.value && analyzer.value) {
         const bufferLength = analyzer.value.frequencyBinCount;
         const dataArray = new Uint8Array(bufferLength);
@@ -41,16 +43,64 @@ export default defineComponent({
         ctx?.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
 
         analyzer.value?.getByteFrequencyData(dataArray);
-        for (let i = 0; i < bufferLength; i++) {
-          const barHeight = dataArray[i];
-          ctx.fillStyle = "white";
-          ctx.fillRect(x, canvasRef.value.height - barHeight, barWidth, barHeight);
-          x += barWidth;
-        }
+
+        drawVisualizer(bufferLength, dataArray, ctx, canvasRef.value);
       }
 
       requestAnimationFrame(animate);
     }
+
+    const drawVisualizer = (
+      bufferLength: number,
+      dataArray: Uint8Array,
+
+      ctx: CanvasRenderingContext2D,
+      canvas: HTMLCanvasElement
+    ) => {
+      //   const style = getComputedStyle(document.body);
+      //   ctx.fillStyle = style.getPropertyValue("--font-color");
+
+      //   let barHeight;
+      //   for (let i = 0; i < bufferLength; i++) {
+      //     barHeight = dataArray[i];
+      //     ctx.fillRect(
+      //       canvas.width / 2 - x.value, // this will start the bars at the center of the canvas and move from right to left
+      //       canvas.height - barHeight,
+      //       barWidth,
+      //       barHeight
+      //     );
+      //     x.value += barWidth; // increases the x value by the width of the bar
+      //   }
+      //   ctx.fillStyle = style.getPropertyValue("--font-color");
+
+      //   for (let i = 0; i < bufferLength; i++) {
+      //     barHeight = dataArray[i];
+      //     ctx.fillRect(x.value, canvas.height - barHeight, barWidth, barHeight); // this will continue moving from left to right
+      //     x.value += barWidth; // increases the x value by the width of the bar
+      //   }
+
+      const style = getComputedStyle(document.body);
+      ctx.fillStyle = style.getPropertyValue("--font-color");
+
+      let barHeight;
+      const barWidth = canvas.width / 2 / bufferLength;
+      let firstX = 0;
+      let secondX = bufferLength * barWidth;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i] - 100;
+        const red = (i * barHeight) / 10;
+        const green = i * 4;
+        const blue = barHeight / 4 - 12;
+        ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+        ctx.fillRect(canvas.width / 2 - firstX, canvas.height - barHeight, barWidth, barHeight);
+        firstX += barWidth;
+        ctx.fillRect(secondX, canvas.height - barHeight, barWidth, barHeight);
+        secondX += barWidth;
+      }
+    };
 
     onBeforeMount(async () => {
       if (props.itemDialog?.path) {
@@ -73,9 +123,6 @@ export default defineComponent({
         analyzer.value.connect(audioCtx.value.destination);
 
         analyzer.value.fftSize = 128;
-        // const bufferLength = analyzer.value.frequencyBinCount;
-        // const dataArray = new Uint8Array(bufferLength);
-        // const barWidth = canvasRef.value.width / bufferLength;
 
         animate();
       }
