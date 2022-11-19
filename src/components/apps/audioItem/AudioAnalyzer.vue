f0ad968b2e95e58c164b56100bd0311fe85cddb2
Redevilkz <stefano.badalucco@gmail.com>
2022-11-19 13:39:50 +0100

    Created component for audio analyzer

---
+++ src/components/apps/audioItem/AudioAnalyzer.vue
@@ -0,0 +1,144 @@
+<template>
+  <canvas id="canvas-audio" ref="canvasRef" class="audio-canvas"></canvas>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, PropType, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    audioElement: Object as PropType<HTMLAudioElement>,
+  },
+  components: {},
+  emits: [],
+  setup(props, _) {
+    const canvasRef = ref<HTMLCanvasElement>();
+
+    const analyzer = ref<AnalyserNode | null>(null);
+    const audioCtx = ref(new window.AudioContext());
+
+    function animate() {
+      if (canvasRef.value && analyzer.value) {
+        const bufferLength = analyzer.value.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+
+        const ctx = canvasRef.value.getContext("2d") as CanvasRenderingContext2D;
+        ctx?.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
+
+        analyzer.value?.getByteFrequencyData(dataArray);
+
+        drawVisualizer(bufferLength, dataArray, ctx, canvasRef.value);
+      }
+
+      requestAnimationFrame(animate);
+    }
+
+    const drawVisualizer = (
+      bufferLength: number,
+      dataArray: Uint8Array,
+
+      ctx: CanvasRenderingContext2D,
+      canvas: HTMLCanvasElement
+    ) => {
+      const style = getComputedStyle(document.body);
+      ctx.fillStyle = style.getPropertyValue("--font-color");
+
+      let barHeight;
+      const barWidth = canvas.width / 2 / bufferLength;
+      let firstX = 0;
+      let secondX = bufferLength * barWidth;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      fixDpi();
+
+      const maxDataArrayValue = 255;
+
+      for (let i = 0; i < bufferLength; i++) {
+        barHeight = (dataArray[i] / maxDataArrayValue) * canvas.height;
+        // const red = (i * barHeight) / 10;
+        // const green = i * 4;
+        // const blue = barHeight / 4 - 12;
+
+        roundRect(ctx, canvas.width / 2 - firstX, (canvas.height - barHeight) / 2, barWidth / 1.7, barHeight, 30);
+        roundRect(ctx, secondX, (canvas.height - barHeight) / 2, barWidth / 1.7, barHeight, 30);
+
+        //ctx.fillRect(canvas.width / 2 - firstX, canvas.height - barHeight, barWidth - 1, barHeight);
+        firstX += barWidth;
+        //ctx.fillRect(secondX, canvas.height - barHeight, barWidth - 1, barHeight);
+        secondX += barWidth;
+      }
+    };
+
+    const fixDpi = () => {
+      const dpi = window.devicePixelRatio;
+
+      if (canvasRef.value) {
+        const height = +getComputedStyle(canvasRef.value).getPropertyValue("height").slice(0, -2);
+        //get CSS width
+        const width = +getComputedStyle(canvasRef.value).getPropertyValue("width").slice(0, -2);
+        //scale the canvas
+        canvasRef.value.setAttribute("height", (height * dpi).toString());
+        canvasRef.value.setAttribute("width", (width * dpi).toString());
+      }
+    };
+
+    const roundRect = function (
+      ctx: CanvasRenderingContext2D,
+      x: number,
+      y: number,
+      width: number,
+      height: number,
+      radius: number
+    ) {
+      if (width < 2 * radius) {
+        radius = width / 2;
+      }
+      if (height < 2 * radius) {
+        radius = height / 2;
+      }
+      const color = getComputedStyle(document.body).getPropertyValue("--font-color");
+
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.arcTo(x + width, y, x + width, y + height, radius);
+      ctx.arcTo(x + width, y + height, x, y + height, radius);
+      ctx.arcTo(x, y + height, x, y, radius);
+      ctx.arcTo(x, y, x + width, y, radius);
+      ctx.closePath();
+
+      ctx.fill();
+    };
+
+    const setAnalyzer = () => {
+      if (props.audioElement && canvasRef.value) {
+        const mediaElement = audioCtx.value.createMediaElementSource(props.audioElement);
+
+        analyzer.value = audioCtx.value.createAnalyser();
+
+        mediaElement.connect(analyzer.value);
+        analyzer.value.connect(audioCtx.value.destination);
+
+        analyzer.value.smoothingTimeConstant = 0.9;
+        analyzer.value.fftSize = 32;
+
+        animate();
+      }
+    };
+
+    onMounted(async () => {
+      setAnalyzer();
+    });
+
+    return { canvasRef };
+  },
+});
+</script>
+
+<style scoped>
+.audio-canvas {
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/components/apps/audioItem/AudioPlayer.vue
+++ src/components/apps/audioItem/AudioPlayer.vue
@@ -2,154 +2,51 @@
   <div :style="`height: ${height}px; width: calc(100% -4px); `" style="position: relative">
     <div id="waveform"></div>
     <div :style="`height: ${height - 80}px; padding: calc(var(--margin)*2) `">
-      <canvas id="canvas-audio" ref="canvasRef" class="audio-canvas"></canvas>
+      <AudioAnalyzer v-if="audioSource" :audioElement="audioRef" />
     </div>
 
-    <audio class="audio-controls" id="audio" controls ref="audioRef" :src="audioSource"></audio>
+    <audio autoplay class="audio-controls" id="audio" controls ref="audioRef" :src="audioSource"></audio>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount, onMounted, PropType, ref } from "vue";
+import { defineComponent, onMounted, PropType, ref } from "vue";
 import ItemDialog from "@/models/ItemDialog";
 import { readFile } from "@/context/fileSystemController";
+
+import AudioAnalyzer from "@/components/apps/audioItem/AudioAnalyzer.vue";
 
 export default defineComponent({
   props: {
     itemDialog: Object as PropType<ItemDialog>,
     height: Number,
   },
-  components: {},
+  components: { AudioAnalyzer },
   emits: [],
   setup(props, _) {
     const canvasRef = ref<HTMLCanvasElement>();
     const audioRef = ref<HTMLAudioElement>();
 
-    const analyzer = ref<AnalyserNode | null>(null);
-    const audioCtx = ref(new window.AudioContext());
     const audioSource = ref();
-    const x = ref(0);
 
-    function animate() {
-      x.value = 0;
-      if (canvasRef.value && analyzer.value) {
-        const bufferLength = analyzer.value.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-        const barWidth = canvasRef.value.width / bufferLength;
-
-        const ctx = canvasRef.value.getContext("2d") as CanvasRenderingContext2D;
-        ctx?.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
-
-        analyzer.value?.getByteFrequencyData(dataArray);
-
-        drawVisualizer(bufferLength, dataArray, ctx, canvasRef.value);
-      }
-
-      requestAnimationFrame(animate);
-    }
-
-    const drawVisualizer = (
-      bufferLength: number,
-      dataArray: Uint8Array,
-
-      ctx: CanvasRenderingContext2D,
-      canvas: HTMLCanvasElement
-    ) => {
-      const style = getComputedStyle(document.body);
-      ctx.fillStyle = style.getPropertyValue("--font-color");
-
-      let barHeight;
-      const barWidth = canvas.width / 2 / bufferLength;
-      let firstX = 0;
-      let secondX = bufferLength * barWidth;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      fixDpi();
-
-      for (let i = 0; i < bufferLength; i++) {
-        console.log(dataArray[i]);
-        barHeight = dataArray[i] * 2;
-        // const red = (i * barHeight) / 10;
-        // const green = i * 4;
-        // const blue = barHeight / 4 - 12;
-
-        roundRect(ctx, canvas.width / 2 - firstX, (canvas.height - barHeight) / 2, barWidth / 1.7, barHeight, 10);
-        roundRect(ctx, secondX, (canvas.height - barHeight) / 2, barWidth / 1.7, barHeight, 10);
-
-        //ctx.fillRect(canvas.width / 2 - firstX, canvas.height - barHeight, barWidth - 1, barHeight);
-        firstX += barWidth;
-        //ctx.fillRect(secondX, canvas.height - barHeight, barWidth - 1, barHeight);
-        secondX += barWidth;
-      }
-    };
-
-    const fixDpi = () => {
-      const dpi = window.devicePixelRatio;
-
-      if (canvasRef.value) {
-        const height = +getComputedStyle(canvasRef.value).getPropertyValue("height").slice(0, -2);
-        //get CSS width
-        const width = +getComputedStyle(canvasRef.value).getPropertyValue("width").slice(0, -2);
-        //scale the canvas
-        canvasRef.value.setAttribute("height", (height * dpi).toString());
-        canvasRef.value.setAttribute("width", (width * dpi).toString());
-      }
-    };
-
-    const roundRect = function (
-      ctx: CanvasRenderingContext2D,
-      x: number,
-      y: number,
-      width: number,
-      height: number,
-      radius: number
-    ) {
-      if (width < 2 * radius) {
-        radius = width / 2;
-      }
-      if (height < 2 * radius) {
-        radius = height / 2;
-      }
-      const color = getComputedStyle(document.body).getPropertyValue("--font-color");
-
-      ctx.fillStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(x + radius, y);
-      ctx.arcTo(x + width, y, x + width, y + height, radius);
-      ctx.arcTo(x + width, y + height, x, y + height, radius);
-      ctx.arcTo(x, y + height, x, y, radius);
-      ctx.arcTo(x, y, x + width, y, radius);
-      ctx.closePath();
-
-      ctx.fill();
-    };
-
-    onBeforeMount(async () => {
+    onMounted(async () => {
       if (props.itemDialog?.path) {
         const audioData = await readFile(props.itemDialog?.path);
-        console.log(audioData);
-
-        //audioSource.value = b64ToText(audioData, true);
         audioSource.value = audioData;
       }
-    });
 
-    onMounted(() => {
-      if (audioRef.value && canvasRef.value) {
-        audioRef.value.play();
-        audioSource.value = audioCtx.value.createMediaElementSource(audioRef.value);
-        console.log(audioSource.value);
-        analyzer.value = audioCtx.value.createAnalyser();
+      //   console.log("DDD", audioSource.value);
+      //   const file = await urltoFile(audioSource.value, "test");
+      //   console.log("FILE", file);
 
-        audioSource.value.connect(analyzer.value);
-        analyzer.value.connect(audioCtx.value.destination);
-
-        analyzer.value.smoothingTimeConstant = 0.9;
-        analyzer.value.fftSize = 32;
-
-        animate();
-      }
+      //   window.jsmediatags.read(file, {
+      //     onSuccess: function (tag: any) {
+      //       console.log("HEY", tag);
+      //     },
+      //     onError: function (error: any) {
+      //       console.log("ERR", error);
+      //     },
+      //   });
     });
 
     return { audioSource, canvasRef, audioRef };
@@ -158,11 +55,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.audio-canvas {
-  height: 100%;
-  width: 100%;
-}
-
 .audio-controls {
   position: absolute;
   bottom: calc(var(--margin) * 2);
