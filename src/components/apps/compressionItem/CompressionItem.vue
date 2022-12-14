79ee90c33f33c26b89c52b3e39e676baf56c64f8
Redevilkz <stefano.badalucco@gmail.com>
2022-12-14 23:21:20 +0100

    Decompress files work

--- src/components/apps/compressionItem/CompressionItem.vue
+++ src/components/apps/compressionItem/CompressionItem.vue
@@ -6,6 +6,9 @@
 import { defineComponent, onBeforeMount, onMounted, PropType } from "vue";
 
 import ItemDialog from "@/models/ItemDialog";
+import * as fflate from "fflate";
+import { readFile } from "@/context/fileSystemController";
+import useBase64Handler from "@/hooks/useBase64Handler";
 
 export default defineComponent({
   props: {
@@ -15,6 +18,24 @@ export default defineComponent({
   components: {},
   emits: [],
   setup(props, _) {
+    const { removeDataUri, b64ToText, isBase64, utf8ToB64 } = useBase64Handler();
+
+    onBeforeMount(async () => {
+      if (props.itemDialog?.path) {
+        let compressed = await readFile(props.itemDialog?.path);
+        console.log("IS BASE64", isBase64(removeDataUri(compressed)));
+        //let buf = fflate.strToU8(compressed);
+        // const buf = Buffer.from(removeDataUri(compressed));
+
+        let buf = Buffer.from(removeDataUri(compressed), "base64");
+        console.log("BUF", buf);
+
+        const decompressed = fflate.unzip(buf, (error, data) => (error ? console.error(error) : console.log(data)));
+
+        console.log("dec", decompressed);
+      }
+    });
+
     return {};
   },
 });
