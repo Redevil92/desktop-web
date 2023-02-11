61e2cde590bc5bac3168c35ab6a08efad09031e2
Redevilkz <stefano.badalucco@gmail.com>
2023-02-11 19:21:32 +0100

    Video player works

--- src/components/apps/videoItem/VideoPlayer.vue
+++ src/components/apps/videoItem/VideoPlayer.vue
@@ -1,35 +1,67 @@
 <template>
-  <div :style="`height: ${height}px; width: calc(100% -4px); `" style="position: relative">
-    <video v-if="videoSource" :src="videoSource" :height="height" width="100%" controls>
-      <!-- <source :src="videoSource" type="video/mp4" /> -->
-      <!-- <source src="movie.ogg" type="video/ogg" /> -->
-      Your browser does not support the video tag.
-    </video>
-  </div>
+  <div
+    ref="player"
+    :style="` height: ${height - 4}px; background-color:black; border-radius:var(--border-radius)`"
+  ></div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, PropType, ref } from "vue";
+import { onMounted, PropType, ref, reactive, nextTick } from "vue";
+import Artplayer from "artplayer";
 import ItemDialog from "@/models/ItemDialog";
 import { readFile } from "@/context/fileSystemController";
-import useBase64Handler from "@/hooks/useBase64Handler";
 
 const props = defineProps({
   itemDialog: Object as PropType<ItemDialog>,
   height: { type: Number, required: true },
 });
 
-const videoSource = ref();
-
-const { removeDataUri } = useBase64Handler();
+const player = ref<any>(null);
+const options = reactive({
+  url: "https://artplayer.org/assets/sample/video.mp4",
+  title: "Your Name",
+  poster: "/assets/sample/poster.jpg",
+  volume: 0.5,
+  isLive: false,
+  muted: false,
+  autoplay: false,
+  pip: true,
+  autoSize: true,
+  autoMini: true,
+  screenshot: true,
+  setting: true,
+  loop: true,
+  flip: true,
+  playbackRate: true,
+  aspectRatio: true,
+  fullscreen: true,
+  fullscreenWeb: true,
+  subtitleOffset: true,
+  miniProgressBar: true,
+  mutex: true,
+  backdrop: true,
+  playsInline: true,
+  autoPlayback: true,
+  airplay: true,
+  theme: "#23ade5",
+  lang: navigator.language.toLowerCase(),
+  whitelist: ["*"],
+  moreVideoAttr: {
+    crossOrigin: "anonymous",
+  },
+});
 
 onMounted(async () => {
-  if (props.itemDialog?.path) {
-    const videoData = await readFile(props.itemDialog?.path);
-    console.log(videoData);
-    videoSource.value = removeDataUri(videoData);
-  }
+  nextTick(async () => {
+    if (player.value && props.itemDialog) {
+      const videoData = await readFile(props.itemDialog?.path);
+      options.url = videoData;
+
+      new Artplayer({
+        ...options,
+        container: player.value,
+      } as any);
+    }
+  });
 });
 </script>
-
-<style scoped></style>
