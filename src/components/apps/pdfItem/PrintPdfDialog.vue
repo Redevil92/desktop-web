cd6df53b0c090cc6c93eb743579dc3857d3db4a0
Redevilkz <stefano.badalucco@gmail.com>
2023-04-28 22:55:00 +0200

    Setup syntax for PrintPdfDialog

--- src/components/apps/pdfItem/PrintPdfDialog.vue
+++ src/components/apps/pdfItem/PrintPdfDialog.vue
@@ -36,58 +36,53 @@
   </BaseDialog>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, onBeforeMount, onMounted, PropType, ref } from "vue";
+<script lang="ts" setup>
+import { onMounted, PropType, ref } from "vue";
 
 import BaseButton from "@/components/shared/BaseButton.vue";
 import BaseInput from "@/components/shared/BaseInput.vue";
 import BaseDialog from "@/components/shared/BaseDialog.vue";
 import BaseRadioButtons from "@/components/shared/BaseRadioButtons.vue";
 
-export default defineComponent({
-  props: {
-    show: Boolean,
-    pdfRef: { type: Object as PropType<HTMLElement | null> },
-  },
-  components: { BaseButton, BaseInput, BaseDialog, BaseRadioButtons },
-  emits: ["close"],
-  setup(props, context) {
-    const name = ref("");
-    const resolution = ref(900);
-    const printOptions = ref(["All the pages", "Current page"]);
-    const selectedPrintOption = ref("Current page");
+const props = defineProps({
+  show: { type: Boolean },
+  pdfRef: { type: Object as PropType<HTMLElement | null> },
+});
 
-    const printPdf = () => {
-      if (selectedPrintOption.value === "All the pages") {
-        // (props.pdfRef as any).pageNums = Array.from(Array((props.pdfRef as any).pageCount).keys());
-      }
-      if (props.pdfRef) {
-        (props.pdfRef as any).print(resolution.value, name.value);
-      }
-    };
+const emit = defineEmits(["close"]);
 
-    const onSelectPrintOptionHandler = (selection: string) => {
-      selectedPrintOption.value = selection;
-    };
+const name = ref("");
+const resolution = ref(900);
+const printOptions = ref(["All the pages", "Current page"]);
+const selectedPrintOption = ref("Current page");
 
-    const closeDialog = () => {
-      context.emit("close");
-    };
+const printPdf = () => {
+  if (selectedPrintOption.value === "All the pages") {
+    // (props.pdfRef as any).pageNums = Array.from(Array((props.pdfRef as any).pageCount).keys());
+  }
+  if (props.pdfRef) {
+    (props.pdfRef as any).print(resolution.value, name.value);
+  }
+};
 
-    onMounted(() => {
-      if (props.pdfRef) {
-        if ((props.pdfRef as any).pageNums.length === (props.pdfRef as any).pageCount) {
-          printOptions.value = ["All the pages"];
-          selectedPrintOption.value = "All the pages";
-        } else {
-          printOptions.value = ["Current page"];
-          selectedPrintOption.value = "Current page";
-        }
-      }
-    });
+const onSelectPrintOptionHandler = (selection: string) => {
+  selectedPrintOption.value = selection;
+};
 
-    return { name, resolution, closeDialog, printPdf, printOptions, selectedPrintOption, onSelectPrintOptionHandler };
-  },
+const closeDialog = () => {
+  emit("close");
+};
+
+onMounted(() => {
+  if (props.pdfRef) {
+    if ((props.pdfRef as any).pageNums.length === (props.pdfRef as any).pageCount) {
+      printOptions.value = ["All the pages"];
+      selectedPrintOption.value = "All the pages";
+    } else {
+      printOptions.value = ["Current page"];
+      selectedPrintOption.value = "Current page";
+    }
+  }
 });
 </script>
 
