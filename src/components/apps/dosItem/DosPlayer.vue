60728219c68ab231ae24ac152016b15ab053d2d9
Redevilkz <stefano.badalucco@gmail.com>
2023-07-14 23:35:56 +0200

    Working on DOS component

--- src/components/apps/dosItem/DosPlayer.vue
+++ src/components/apps/dosItem/DosPlayer.vue
@@ -1,13 +1,14 @@
 <template>
   <div :style="`height: ${height}px; width: calc(100% -4px); `">
-    <IFrameFocuser v-if="!itemDialog.isFocused" :height="height - 4" />
+    <!-- <IFrameFocuser v-if="!itemDialog.isFocused" :height="height - 4" />
     <iframe
       class="frame"
       :height="height - 4"
       :width="itemDialog.dimension.width - 4"
       title="dos-player"
       :src="gameLink"
-    />
+    /> -->
+    <div style="height: 100%; width: 100%" ref="rootRef"></div>
   </div>
 </template>
 
@@ -19,18 +20,22 @@ import IFrameFocuser from "@/components/shared/IFrameFocuser.vue";
 import { onMounted, PropType, ref } from "vue";
 import { dosGameLinks } from "@/components/apps/dosItem/DosGames";
 
+import { DosPlayer as Instance, DosPlayerFactoryType } from "js-dos";
+
 const props = defineProps({
   itemDialog: { type: Object as PropType<ItemDialog>, required: true },
   height: { type: Number, required: true },
 });
 
+const rootRef = ref<HTMLDivElement | null>(null);
+
 const gameLink = ref("");
 
 onMounted(() => {
-  if (props.itemDialog?.name) {
-    props.itemDialog.isFocused;
-    gameLink.value = dosGameLinks[props.itemDialog.name];
-  }
+  // if (props.itemDialog?.name) {
+  //   props.itemDialog.isFocused;
+  //   gameLink.value = dosGameLinks[props.itemDialog.name];
+  // }
 });
 </script>
 
