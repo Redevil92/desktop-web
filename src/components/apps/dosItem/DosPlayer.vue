1af49d3b0006e33ec28d37b260c79de657894cae
Redevilkz <stefano.badalucco@gmail.com>
2023-07-30 22:12:08 +0200

    Back to previous dos emulator

--- src/components/apps/dosItem/DosPlayer.vue
+++ src/components/apps/dosItem/DosPlayer.vue
@@ -1,5 +1,50 @@
 <template>
   <div :style="`height: ${height}px; width: calc(100% -4px); `">
+    <IFrameFocuser v-if="!itemDialog.isFocused" :height="height - 4" />
+    <iframe
+      class="frame"
+      :height="height - 4"
+      :width="itemDialog.dimension.width - 4"
+      title="dos-player"
+      :src="gameLink"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ItemDialog from "@/models/ItemDialog";
+
+import IFrameFocuser from "@/components/shared/IFrameFocuser.vue";
+
+import { onMounted, PropType, ref } from "vue";
+import { dosGameLinks } from "@/components/apps/dosItem/DosGames";
+
+const props = defineProps({
+  itemDialog: { type: Object as PropType<ItemDialog>, required: true },
+  height: { type: Number, required: true },
+});
+
+const gameLink = ref("");
+
+onMounted(() => {
+  if (props.itemDialog?.name) {
+    props.itemDialog.isFocused;
+    gameLink.value = dosGameLinks[props.itemDialog.name];
+  }
+});
+</script>
+
+<style scoped>
+.frame {
+  border-bottom-left-radius: var(--border-radius);
+  border-bottom-right-radius: var(--border-radius);
+}
+</style>
+
+<!-- NEW VERSION WITH DOS EMULATOR -->
+
+<!-- <template>
+  <div :style="`height: ${height}px; width: calc(100% -4px); `">
     <div style="height: 100%; width: 100%" ref="rootRef"></div>
   </div>
 </template>
@@ -43,4 +88,4 @@ onMounted(() => {
   border-bottom-left-radius: var(--border-radius);
   border-bottom-right-radius: var(--border-radius);
 }
-</style>
+</style> -->
