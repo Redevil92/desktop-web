b412f0fd4009b3651a595f392bb1bdf4a72c78e0
Redevilkz <stefano.badalucco@gmail.com>
2024-02-13 22:38:30 +0100

    Created food recipe collections file.

---
+++ src/components/apps/foodRecipeCollections.vue/FoodRecipeCollections.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Your template code here -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+// Your script code here
+</script>
+
+<style scoped>
+/* Your style code here */
+</style>
