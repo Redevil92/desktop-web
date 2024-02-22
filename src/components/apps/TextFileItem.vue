5964e24dc7afb939b0a38bd65d0be07167fe048f
Redevilkz <stefano.badalucco@gmail.com>
2024-02-22 22:02:57 +0100

    [WIP] Implemented texteditor with Quill

--- src/components/apps/TextFileItem.vue
+++ src/components/apps/TextFileItem.vue
@@ -7,8 +7,10 @@
     :showSaveAsInput="true"
   ></SelectedFolderDialog>
   <IFrameFocuser v-if="!itemDialog.isFocused" :height="height - 5" />
-  <div id="textFileItem" :style="`height: ${height - 5}px; width: calc(100% -4px);`">
-    <Editor
+  <div style="background-color: white">
+    <div id="textFileItem" :style="`height: ${height}px;  width: calc(100% -4px); `">
+      <div id="editor" style="background-color: white; border-radius: 10px; height: 100%"></div>
+      <!-- <Editor
       v-if="isLoaded"
       class="mce-editor"
       api-key="yxb2ealwgpgr85gcgcl311khnyuz4abs13akcuyqscr4y6fr"
@@ -24,16 +26,20 @@
       }"
       :initial-value="fileText"
       @saveContent="saveFile"
-    />
+    /> -->
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeMount, PropType, ref } from "vue";
+import { onBeforeMount, onMounted, PropType, ref } from "vue";
 
 import ItemDialog from "@/models/ItemDialog";
 import SelectedFolderDialog from "@/components/shared/SelectedFolderDialog.vue";
 import IFrameFocuser from "@/components/shared/IFrameFocuser.vue";
+
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
 import Editor from "@tinymce/tinymce-vue";
 import fileSystem from "@/context/fileSystemController";
@@ -56,27 +62,29 @@ const layoutStore = useLayoutStore();
 const fileSystemStore = useFileSystemStore();
 const { isBase64, b64ToText, utf8ToB64, removeDataUri } = useBase64Handler();
 
+const quill = ref<Quill>();
 const fileText = ref("");
 const isLoaded = ref(false);
 const showSaveAsDialog = ref(false);
 const fileContent = ref("");
 
-const saveFile = (content: any, _html: any, _body: any) => {
+const saveFile = (_html: any, _body: any) => {
   if (props.itemDialog?.path) {
+    const content = quill.value?.getText();
     fileSystemStore.updateFile({
       path: props.itemDialog?.path,
-      content: utf8ToB64(content.content),
+      content: utf8ToB64(content ?? ""),
     } as PathAndContent);
   } else {
-    fileContent.value = content.content;
     showSaveAsDialog.value = true;
   }
 };
 
 const saveTextFileHandler = async (destinationPath: string) => {
   const destinationPathToSave = destinationPath + ".txt";
+  const content = quill.value?.getText();
 
-  await fileSystemStore.createFile({ path: destinationPathToSave, content: utf8ToB64(fileContent.value) });
+  await fileSystemStore.createFile({ path: destinationPathToSave, content: utf8ToB64(content ?? "") });
   showSaveAsDialog.value = false;
 
   const itemDialogToUpdate = { ...props.itemDialog };
@@ -93,10 +101,56 @@ const saveTextFileHandler = async (destinationPath: string) => {
   });
 };
 
+onMounted(() => {
+  const container = document.getElementById("editor");
+  console.log(container);
+
+  if (!container) return;
+  const toolbarOptions = [
+    [{ font: [] }, "bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "image", "video"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ];
+
+  quill.value = new Quill(container, {
+    theme: "snow",
+    modules: {
+      toolbar: toolbarOptions,
+    },
+  });
+
+  // quill.value.on("text-change", (delta, oldDelta, source) => {
+  //   console.log("change!", delta, oldDelta, source);
+  //   if (source == "api") {
+  //     console.log("An API call triggered this change.");
+  //   } else if (source == "user") {
+  //     console.log("A user action triggered this change.");
+  //   }
+  // });
+});
+
 onBeforeMount(async () => {
   if (props.itemDialog?.path) {
     let fileData = await fileSystem.readFile(props.itemDialog?.path);
     fileText.value = b64ToText(fileData, true);
+    if (quill.value) {
+      quill.value.setText(fileText.value);
+    }
 
     isLoaded.value = true;
   } else {
@@ -105,3 +159,4 @@ onBeforeMount(async () => {
   }
 });
 </script>
+<style></style>
