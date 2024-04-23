11555a827ee2b5dc857e9f5f1726193060331735
Redevilkz <stefano.badalucco@gmail.com>
2024-04-23 21:45:23 +0200

    Added badge for debug mode.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
+  <div class="debud-mode-flag" v-if="isDebugMode">DEBUG MODE ACTIVE</div>
   <router-view />
 </template>
+
+<script setup lang="ts">
+import { computed, PropType } from 'vue';
+
+const isDebugMode = computed(() => import.meta.env.VITE_DEBUG_MODE === 'true');
+</script>
 
 <style>
 @import '../public/index.css';
@@ -24,5 +31,18 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.debud-mode-flag {
+  position: fixed;
+  right: 20px;
+  top: 20px;
+  z-index: 200;
+  background-color: #ffecec;
+  padding: 5px 10px;
+  border: 2px solid #db5353;
+  border-radius: 5px;
+  font-size: var(--medium-font-size);
+  font-weight: 600;
 }
 </style>
