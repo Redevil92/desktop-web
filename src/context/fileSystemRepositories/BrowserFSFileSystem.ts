dffa58ffb5a4b64ba568e8a25aeef118208d898b
Redevilkz <stefano.badalucco@gmail.com>
2024-04-23 22:19:26 +0200

    Handled exeption for isDir method.

--- src/context/fileSystemRepositories/BrowserFSFileSystem.ts
+++ src/context/fileSystemRepositories/BrowserFSFileSystem.ts
@@ -1,6 +1,6 @@
-import { IFileSystem } from './IFileSystem'
-import { DESKTOP_PATH } from '@/constants'
-import FileStats from '@/models/FileSystem/FileStats'
+import { IFileSystem } from './IFileSystem';
+import { DESKTOP_PATH } from '@/constants';
+import FileStats from '@/models/FileSystem/FileStats';
 import {
   generateUniqueName,
   getFileExtensionFromName,
@@ -8,43 +8,43 @@ import {
   getFileNameWithoutExtension,
   getSourcePathFromFilePath,
   MIME_TYPES
-} from '../utils/fileSystemUtils'
-import { BFSRequire } from 'browserfs'
+} from '../utils/fileSystemUtils';
+import { BFSRequire } from 'browserfs';
 
 export default class BrowserFSFileSystem implements IFileSystem {
   ensureDirectoryExistence = async (filePath: string) => {
-    const dirname = getSourcePathFromFilePath(filePath)
+    const dirname = getSourcePathFromFilePath(filePath);
 
     if ((await this.existsFile(dirname)) || !dirname) {
-      return true
+      return true;
     }
-    await this.ensureDirectoryExistence(dirname)
-    await this.createDirectory(dirname)
-  }
+    await this.ensureDirectoryExistence(dirname);
+    await this.createDirectory(dirname);
+  };
 
   createDirectory = async (
     path: string,
     overwrite = false,
     overwriteIfSameName = false
   ): Promise<string> => {
-    const fs = BFSRequire('fs')
-    await this.ensureDirectoryExistence(path)
-    let uniquePath = path
+    const fs = BFSRequire('fs');
+    await this.ensureDirectoryExistence(path);
+    let uniquePath = path;
 
     if (!overwriteIfSameName) {
-      const exists = await this.existsFile(path)
+      const exists = await this.existsFile(path);
       if (exists) {
-        const destinationPathFileList = await this.getFiles(getSourcePathFromFilePath(path), true)
-        uniquePath = generateUniqueName(path, destinationPathFileList)
+        const destinationPathFileList = await this.getFiles(getSourcePathFromFilePath(path), true);
+        uniquePath = generateUniqueName(path, destinationPathFileList);
       }
     }
 
     return new Promise((resolve, reject) => {
       fs.mkdir(uniquePath, { flag: overwrite ? 'w' : 'wx' }, (error: any) =>
         error ? reject(error) : resolve(uniquePath)
-      )
-    })
-  }
+      );
+    });
+  };
 
   createFile = async (
     path: string,
@@ -52,162 +52,166 @@ export default class BrowserFSFileSystem implements IFileSystem {
     encoding = 'utf8',
     overwriteIfSameName = true
   ): Promise<string> => {
-    const fs = BFSRequire('fs')
-    await this.ensureDirectoryExistence(path)
+    const fs = BFSRequire('fs');
+    await this.ensureDirectoryExistence(path);
 
-    let uniqueFilePath = path
+    let uniqueFilePath = path;
     if (!overwriteIfSameName) {
-      const exists = await this.existsFile(path)
+      const exists = await this.existsFile(path);
       if (exists) {
-        const destinationPathFileList = await this.getFiles(getSourcePathFromFilePath(path), true)
+        const destinationPathFileList = await this.getFiles(getSourcePathFromFilePath(path), true);
         uniqueFilePath = generateUniqueName(
           getFileNameWithoutExtension(path),
           destinationPathFileList
-        )
-        uniqueFilePath = uniqueFilePath + '.' + getFileExtensionFromName(path)
+        );
+        uniqueFilePath = uniqueFilePath + '.' + getFileExtensionFromName(path);
       }
     }
 
     return new Promise((resolve, reject) => {
       fs.writeFile(uniqueFilePath, text, encoding, (error: any) => {
-        error && error.code !== 'EEXIST' ? reject(error) : resolve(uniqueFilePath)
-      })
-    })
-  }
+        error && error.code !== 'EEXIST' ? reject(error) : resolve(uniqueFilePath);
+      });
+    });
+  };
 
   existsFile = (path: string): Promise<boolean> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
 
     return new Promise((resolve) => {
-      fs.exists(path, resolve)
-    })
-  }
+      fs.exists(path, resolve);
+    });
+  };
 
   renameFile = (newFilePath: string, oldFilePath: string): Promise<any> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
 
     return new Promise((resolve, reject) => {
-      fs.rename(oldFilePath, newFilePath, (error: any) => (error ? reject(error) : resolve(true)))
-    })
-  }
+      fs.rename(oldFilePath, newFilePath, (error: any) => (error ? reject(error) : resolve(true)));
+    });
+  };
 
   getFiles = async (path: string, fullPath = false): Promise<string[]> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
     const filesPromise: string[] = await new Promise((resolve, reject) => {
-      fs.readdir(path, (error: any, res: string[]) => (error ? reject(error) : resolve(res)))
-    })
+      fs.readdir(path, (error: any, res: string[]) => (error ? reject(error) : resolve(res)));
+    });
 
     if (fullPath) {
-      return filesPromise.map((file) => path + '/' + file)
+      return filesPromise.map((file) => path + '/' + file);
     }
 
-    return filesPromise
-  }
+    return filesPromise;
+  };
 
   getDesktopFiles = (fullPath = false): Promise<string[]> => {
-    return this.getFiles(DESKTOP_PATH, fullPath)
-  }
+    return this.getFiles(DESKTOP_PATH, fullPath);
+  };
 
   isDir = async (path: string): Promise<boolean> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
 
-    const fileStat: any = await new Promise((resolve, reject) => {
-      fs.stat(path, (error: any, res: any) => (error ? reject(error) : resolve(res)))
-    })
+    try {
+      const fileStat: any = await new Promise((resolve, reject) => {
+        fs.stat(path, (error: any, res: any) => (error ? reject(error) : resolve(res)));
+      });
 
-    return fileStat.isDirectory()
-  }
+      return fileStat.isDirectory();
+    } catch (error) {
+      return false;
+    }
+  };
 
   deleteFileSystemItem = async (path: string) => {
-    const isFolder = await this.isDir(path)
+    const isFolder = await this.isDir(path);
     if (isFolder) {
-      await this.deleteFolder(path)
+      await this.deleteFolder(path);
     } else {
-      await this.deleteFile(path)
+      await this.deleteFile(path);
     }
-  }
+  };
 
   deleteFile = (filePath: string): Promise<any> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
     return new Promise((resolve, reject) => {
-      fs.unlink(filePath, (error: any) => (error ? reject(error) : resolve(true)))
-    })
-  }
+      fs.unlink(filePath, (error: any) => (error ? reject(error) : resolve(true)));
+    });
+  };
 
   deleteFolder = async (path: string) => {
-    const isFolder = await this.isDir(path)
+    const isFolder = await this.isDir(path);
     if (isFolder) {
-      const dirContents = await this.getFiles(path, true)
+      const dirContents = await this.getFiles(path, true);
       for (const content of dirContents) {
-        await this.deleteFolder(content)
+        await this.deleteFolder(content);
       }
-      await this.deleteEmptyFolder(path)
+      await this.deleteEmptyFolder(path);
     } else {
-      this.deleteFile(path)
+      this.deleteFile(path);
     }
-  }
+  };
 
   deleteEmptyFolder = async (path: string) => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
 
     return new Promise((resolve, reject) => {
-      fs.rmdir(path, (error: any, res: FileStats) => (error ? reject(error) : resolve(res)))
-    })
-  }
+      fs.rmdir(path, (error: any, res: FileStats) => (error ? reject(error) : resolve(res)));
+    });
+  };
 
   getStat = (path: string): Promise<FileStats> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
 
     return new Promise((resolve, reject) => {
-      fs.stat(path, (error: any, res: FileStats) => (error ? reject(error) : resolve(res)))
-    })
-  }
+      fs.stat(path, (error: any, res: FileStats) => (error ? reject(error) : resolve(res)));
+    });
+  };
 
   readFile = async (path: string, encoding = 'utf8'): Promise<string> => {
-    const fs = BFSRequire('fs')
+    const fs = BFSRequire('fs');
     const fileToRead: any = await new Promise((resolve, reject) => {
-      fs.readFile(path, (error: any, res: any) => (error ? reject(error) : resolve(res)))
-    })
+      fs.readFile(path, (error: any, res: any) => (error ? reject(error) : resolve(res)));
+    });
 
-    return fileToRead.toString(encoding)
-  }
+    return fileToRead.toString(encoding);
+  };
 
   copyFolder = async (filePath: string, destinationPath: string) => {
-    const destinationPathFileList = await this.getFiles(destinationPath)
+    const destinationPathFileList = await this.getFiles(destinationPath);
     const uniqueFolderName = generateUniqueName(
       getFileNameFromPath(filePath),
       destinationPathFileList
-    )
-    const newFolderPath = destinationPath + '/' + uniqueFolderName
-    await this.createDirectory(newFolderPath)
-    const filesName = await this.getFiles(filePath, true)
+    );
+    const newFolderPath = destinationPath + '/' + uniqueFolderName;
+    await this.createDirectory(newFolderPath);
+    const filesName = await this.getFiles(filePath, true);
     for (const file of filesName) {
-      await this.copyFileSystemItem(file, newFolderPath)
+      await this.copyFileSystemItem(file, newFolderPath);
     }
-  }
+  };
 
   copyFile = async (filePath: string, destinationPath: string) => {
-    const fileData = await this.readFile(filePath)
-    const filesName = await this.getFiles(destinationPath, true)
+    const fileData = await this.readFile(filePath);
+    const filesName = await this.getFiles(destinationPath, true);
 
     const nameToCheck = getFileNameWithoutExtension(
       destinationPath + '/' + getFileNameFromPath(filePath)
-    )
-    const extension = getFileExtensionFromName(filePath)
-    const uniqueFilePath = generateUniqueName(nameToCheck, filesName) + `.${extension}`
+    );
+    const extension = getFileExtensionFromName(filePath);
+    const uniqueFilePath = generateUniqueName(nameToCheck, filesName) + `.${extension}`;
 
-    await this.createFile(uniqueFilePath, fileData)
-  }
+    await this.createFile(uniqueFilePath, fileData);
+  };
 
   copyFileSystemItem = async (filePath: string, destinationPath: string) => {
-    const isDirectory = await this.isDir(filePath)
+    const isDirectory = await this.isDir(filePath);
 
     if (isDirectory) {
-      await this.copyFolder(filePath, destinationPath)
+      await this.copyFolder(filePath, destinationPath);
     } else {
-      await this.copyFile(filePath, destinationPath)
+      await this.copyFile(filePath, destinationPath);
     }
-  }
+  };
 
   moveFiles = async (filesToMove: string[], destinationPath: string, keepOriginal = false) => {
     for (const filePath of filesToMove) {
@@ -215,29 +219,29 @@ export default class BrowserFSFileSystem implements IFileSystem {
       //   break;
       // }
 
-      await this.copyFileSystemItem(filePath, destinationPath)
+      await this.copyFileSystemItem(filePath, destinationPath);
       if (!keepOriginal) {
-        await this.deleteFileSystemItem(filePath)
+        await this.deleteFileSystemItem(filePath);
       }
     }
-  }
+  };
 
   downloadFiles = async (filesPath: string[]) => {
     //TODO: if multiple filesPath -> create a zip file and then download
     for (const filePath of filesPath) {
-      const downloadLink = document.createElement('a')
-      document.body.appendChild(downloadLink)
-      let fileData = await this.readFile(filePath)
+      const downloadLink = document.createElement('a');
+      document.body.appendChild(downloadLink);
+      let fileData = await this.readFile(filePath);
 
       if (!fileData.startsWith('data:')) {
-        const mimeType = MIME_TYPES[getFileExtensionFromName(filePath)]
-        fileData = `data:${mimeType};base64,${fileData}`
+        const mimeType = MIME_TYPES[getFileExtensionFromName(filePath)];
+        fileData = `data:${mimeType};base64,${fileData}`;
       }
 
-      downloadLink.href = fileData
-      downloadLink.target = '_self'
-      downloadLink.download = getFileNameFromPath(filePath)
-      downloadLink.click()
+      downloadLink.href = fileData;
+      downloadLink.target = '_self';
+      downloadLink.download = getFileNameFromPath(filePath);
+      downloadLink.click();
     }
-  }
+  };
 }
