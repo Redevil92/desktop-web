d2cc3ecdaddb1a478d2fd83bd1b17f9e4d6099fe
Redevil92 <stefano.badalucco@gmail.com>
2024-06-29 15:59:45 +0200

    Commented out rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -13,17 +13,17 @@ export default defineConfig({
   },
   build: {
     commonjsOptions: { transformMixedEsModules: true }, // Change
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    }
+    // rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ['vue'],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: 'Vue',
+    //     },
+    //   },
+    // }
   }
 })
