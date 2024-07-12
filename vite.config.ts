ea192c244ee71deac92d232c48c49b927cc8be90
Redevil92 <stefano.badalucco@gmail.com>
2024-07-12 14:53:14 +0200

    Updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dynamicImportVars({})],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
