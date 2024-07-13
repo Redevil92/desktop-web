4c285872a159d435e17fb861a9aafb0fb8d1338d
Redevil92 <stefano.badalucco@gmail.com>
2024-07-13 19:32:47 +0200

    Added node polyfills for Buffer.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), nodePolyfills()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
